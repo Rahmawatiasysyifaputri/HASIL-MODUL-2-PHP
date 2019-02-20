2efd0cb563c1bc566da8547b01330b65735e1dfe
Rahmawati Asy syifa' Putri 27 RPL <42015213+Rahmawatiasysyifaputri@users.noreply.github.com>
2019-02-20 15:01:49 +0700

    Add files via upload

---
+++ LT 6.php
@@ -0,0 +1,12 @@
+<?php
+     $f=4;
+     $k=9;
+ 
+     if ($f == 4 || $k == 9) {
+         
+         echo "<br><h1>Data benar</h1>";
+     } else  {
+ 
+         echo "<br><h1>Data Salah </h1>";
+     }
+ ?>
